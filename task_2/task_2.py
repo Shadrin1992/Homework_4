e359cfa6b88077b7a78731b321e8abb36883a63f
Evgeny <shadrin_pharm@mail.ru>
2022-12-15 14:25:40 +0300

    Решение

---
+++ task_2/task_2.py
@@ -0,0 +1,19 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+# * 6 -> [2,3]
+# * 144 -> [2,3]
+
+def Factor(n):
+    result = []
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            result.append(d)
+            n //= d
+        else:
+            d += 1
+    if n > 1:
+        result.append(n)
+    return result
+
+result = Factor(int(input()))
+print(list(set(result)))
