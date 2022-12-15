682ae1cf89e871cdddb24c1ec2984d1c7ae0e180
Evgeny <shadrin_pharm@mail.ru>
2022-12-15 14:42:48 +0300

    Решение

--- task_3/task_3.py
+++ task_3/task_3.py
@@ -3,7 +3,9 @@
 
 from random import randint
 
-lst = [randint(1, 5) for i in range(15)]
+lst = [randint(1, 7) for i in range(15)]
 print(lst)
-
 print(list(set(lst)))
+
+lst = [i for i in lst if lst.count(i) == 1]
+print(f'Список неповторяющихся элементов: {lst}')
